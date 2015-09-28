ef2eda88ecabfe4f8e193730258e387309471639
glasswm <glasswm@163.com>
2015-09-28 18:38:06 +0800

    ADD: complete get account data from mysql MOD: refactor

--- main.py
+++ main.py
@@ -10,36 +10,19 @@ from common import DatabaseType, EncryptAlgorithmType
 from models import SystemInfo, Base, DBUtil
 
 
-def db_init():
-    sysinfo_db_engine = create_engine('sqlite:///' + LOCAL_DB_File, echo=False)
-    Base.metadata.create_all(sysinfo_db_engine)
-    Session = sessionmaker(bind=sysinfo_db_engine)
-    session = Session()
-    demo_system = SystemInfo(sys_name='oa', db_type=DatabaseType.oracle, db_ip='127.0.0.1',
-                         db_port='1521', db_name='db', db_table_name='accounts', db_column_username='username',
-                         db_column_password='password', db_password_encrypt_algorithm=EncryptAlgorithmType.md5)
-    session.add(demo_system)
-    session.commit()
-
-
 def test():
-    engine = create_engine('mysql+mysqlconnector://root:7410@192.168.44.133:3306/wpd', echo=False)
-    conn = engine.connect()
-    metadata = MetaData()
-
-    users = Table('user_table', metadata,
-                  Column('user_id', String),
-                  Column('user_password', String),
-                  )
-
-    s = select([users])
-    result = conn.execute(s)
-    for row in result:
-        print row
-    result.close()
-
-if __name__ == '__main__':
     db_util = DBUtil()
+    id = 12
+    print '____________________%d' % id
+    s =  db_util.get_system_by_id(id)
+    print s
+    s.get_account_data(username='root', password='7410')
+
+
+def demotest():
+    db_util = DBUtil()
+
+    #create
     demo_system = SystemInfo(sys_name='oa333', db_type=DatabaseType.oracle, db_ip='127.0.0.1',
                          db_port='1521', db_name='db', db_table_name='accounts', db_column_username='username',
                          db_column_password='password', db_password_encrypt_algorithm=EncryptAlgorithmType.md5)
@@ -47,15 +30,21 @@ if __name__ == '__main__':
     for i in db_util.get_all_system():
         print i
 
+    #update
     demo_system.db_type = DatabaseType.mysql
     demo_system.db_port = '3306'
 
+    #request
     idd = demo_system.id
-
     print '____________________%d' % idd
     s =  db_util.get_system_by_id(idd)
     print s
     print '__ __ __ __ __ __ __ ___ ___%d' % idd
+
+    #delete
     db_util.del_system(s)
     s =  db_util.get_system_by_id(idd)
     print s
+
+if __name__ == '__main__':
+    test()
--- models.py
+++ models.py
@@ -1,12 +1,12 @@
-from sqlalchemy.orm import sessionmaker
-from __setting__ import LOCAL_DB_File
-
 __author__ = 'wm'
 
-from sqlalchemy import Column, Integer, Sequence, String, create_engine
+from sqlalchemy import Column, Integer, Sequence, String, create_engine, MetaData, Table, select
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy_enum34 import EnumType
 from common import DatabaseType, EncryptAlgorithmType, singleton
+from sqlalchemy.orm import sessionmaker
+from __setting__ import LOCAL_DB_File
+import logging
 
 Base = declarative_base()
 
@@ -24,7 +24,39 @@ class SystemInfo(Base):
     db_password_encrypt_algorithm = Column(EnumType(EncryptAlgorithmType), nullable=False)
 
     def __repr__(self):
-       return "<SystemInfo(id=%d, sys_name='%s', db_type='%s', db_name='%s')>" % (self.id, self.sys_name, self.db_type, self.db_name)
+        return "<SystemInfo(id=%d, sys_name='%s', db_type='%s', db_name='%s')>" % (self.id, self.sys_name, self.db_type, self.db_name)
+
+
+    def get_account_data(self, username, password):
+        # Database Urls = "dialect+driver://username:password@host:port/database"
+        if (self.db_type == DatabaseType.mysql):
+            dialect = 'mysql'
+            driver = 'mysqlconnector'
+        elif (self.db_type == DatabaseType.oracle):
+            dialect = 'oracle'
+            driver = ''
+        else:
+            logging.error(('unknown database type %s' % self.db_type))
+            exit()
+        sql_schema = '%s+%s://%s:%s@%s:%s/%s' % (dialect, driver, username, password, self.db_ip, self.db_port, self.db_name)
+        engine = create_engine(sql_schema, echo=False)
+        conn = engine.connect()
+        metadata = MetaData()
+
+        users = Table(self.db_table_name, metadata,
+                      Column(self.db_column_username, String),
+                      Column(self.db_column_password, String),
+                      )
+
+        s = select([users])
+        result = conn.execute(s)
+        res = []
+        for row in result:
+            #print row[self.db_column_username], row[self.db_column_password]
+            res.append((row[self.db_column_username], row[self.db_column_password]))
+        result.close()
+        print res
+        return res
 
 
 @singleton
