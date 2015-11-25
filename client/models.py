dc09c7bfa203748c50fd1420437cf5270f3eaa21
glasswm <glasswm@163.com>
2015-11-25 21:08:44 +0800

    ADD: crypt_pass  set&get

--- client/models.py
+++ client/models.py
@@ -1,10 +1,11 @@
 __author__ = 'wm'
 
-from sqlalchemy import Column, Integer, Sequence, String, create_engine, MetaData, Table, select
+from sqlalchemy import Column, Integer, Sequence, String, create_engine, MetaData, Table, select, ForeignKey, \
+    UniqueConstraint
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy_enum34 import EnumType
 from common import DatabaseType, EncryptAlgorithmType, singleton
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship, backref
 from __setting__ import LOCAL_DB_File
 import logging
 
@@ -58,6 +59,19 @@ class SystemInfo(Base):
         print res
         return res
 
+class Cryptograph(Base):
+    __tablename__ = 'cryptograph'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String, nullable=False)
+    crypt_pass = Column(String, nullable=False)
+    last_time = Column(Integer, nullable=False, default=0)
+    system_id = Column(Integer, ForeignKey('systems.id'))
+
+    system = relationship("SystemInfo", backref=backref('cryptograph', order_by=id))
+    UniqueConstraint('user_name', 'system', name='uix_1')
+
+    def __repr__(self):
+        return "<Cryptograph(%s %s %d)>" % self.user_name, self.crypt_pass, self.last_time
 
 @singleton
 class DBUtil(object):
@@ -95,5 +109,31 @@ class DBUtil(object):
         self.session.delete(sys_info)
         self.session.commit()
 
+    def get_crypt_by_systemID(self, sys_id):
+        sys_info = self.session.query(SystemInfo).filter(SystemInfo.id==sys_id).first()
+        crypt_list = []
+        for i in sys_info.cryptograph:
+            crypt_list.append((i.user_name, i.crypt_pass, i.last_time))
+        return crypt_list
+
+    #crypt_list = [(user_name1, crypt_pass1, last_time1), (user_name2, crypt_pass2, last_time2), ...]
+    def set_crypt(self, sys_id, crypt_list):
+        sys_info = self.session.query(SystemInfo).filter(SystemInfo.id==sys_id).first()
+        for i in crypt_list:
+            rec = self.session.query(Cryptograph).join(SystemInfo).filter(SystemInfo.id==sys_id, Cryptograph.user_name==i[0]).first()
+            if rec != None:
+                rec.crypt_pass = i[1]
+                rec.last_time = i[2]
+            else:
+                rec = Cryptograph(user_name=i[0], crypt_pass=i[1], last_time=i[2], system=sys_info)
+                self.session.add(rec)
+            self.session.commit()
+
     def __del__(self):
         self.session.close()
+
+if __name__ == "__main__":
+    db_util = DBUtil()
+    crypt_list = [('user1', '1242dsdf341', 0), ('user2', '1242dsdf342', 1), ('user3', '133342dsdf341', 10)]
+    db_util.set_crypt(12, crypt_list)
+    print db_util.get_crypt_by_systemID(12)
