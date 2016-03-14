118ce9bfffd471536aa42fdcd74fc4f60e1b8043
glasswm <glasswm@163.com>
2016-03-14 19:56:39 +0800

    MOD: move conf from setting.py to cfg file

--- client/__setting__.py
+++ client/__setting__.py
@@ -1,13 +1,21 @@
 __author__ = 'glasswm'
 
-LOCAL_DB_File = '../tmp.db'
 
-wp_server = 'http://192.168.238.132:4000/wpd'
+import ConfigParser
 
+
+config = ConfigParser.RawConfigParser()
+config.read('main.cfg')
+
+LOCAL_DB_File = config.get('Crack', 'local_db')
+
+#wp_server = 'http://192.168.238.132:4000/wpd'
+wp_server = config.get('Crack', 'wp_server')
 
 proxies = {
   "http": "http://127.0.0.1:8080",
   "https": "http://127.0.0.1:8080",
 }
 
-detect_period = 5
+#detect_period = 10
+detect_period = config.get('Crack', 'detect_period')
